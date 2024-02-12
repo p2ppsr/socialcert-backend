3c9c69f8257770c87a816394a5577c1d5882fab0
Brayden Langley <brayden167@gmail.com>
2024-02-12 08:40:14 -0800

    Added missing dep

--- src/routes/checkVerification.js
+++ src/routes/checkVerification.js
@@ -3,6 +3,7 @@
 // const getPhotoDataAsBuffer = require('../utils/getPhotoDataAsBuffer')
 // const { getVerificationInfo } = require('../utils/getVerificationInfo')
 // const { publishFile } = require('nanostore-publisher')
+const axios = require('axios')
 
 const {
   NANOSTORE_URL,
@@ -30,80 +31,80 @@ module.exports = {
   func: async (req, res) => {
     try {
       if (!req.body.preVerifiedData || !req.body.preVerifiedData.accessCode || req.body.preVerifiedData === 'notVerified') {
-        return res.status(400).json({ 
+        return res.status(400).json({
           status: 'notVerified',
           description: 'User identity has not been verified!'
         })
       }
 
-    
-        const data = new URLSearchParams({
-          'grant_type': 'authorization_code',
-          'code': req.body.preVerifiedData.accessCode,
-          'redirect_uri': REDIRECT_URI
-        });
-      
-        const headers = {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        };
 
-        let authResponse = await axios.post(`${DISCORD_API_ENDPOINT}/oauth2/token`, data, {
-          headers: headers,
-          auth: {
-            username: DISCORD_CLIENT_ID,
-            password: DISCORD_CLIENT_SECRET
-          }
+      const data = new URLSearchParams({
+        'grant_type': 'authorization_code',
+        'code': req.body.preVerifiedData.accessCode,
+        'redirect_uri': REDIRECT_URI
+      });
+
+      const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      };
+
+      let authResponse = await axios.post(`${DISCORD_API_ENDPOINT}/oauth2/token`, data, {
+        headers: headers,
+        auth: {
+          username: DISCORD_CLIENT_ID,
+          password: DISCORD_CLIENT_SECRET
+        }
+      })
+
+      let access_token = authResponse.data.access_token;
+      const dataResponse = await axios.get(`${DISCORD_API_ENDPOINT}/oauth2/@me`, { headers: { 'Authorization': `Bearer ${access_token}` } });
+
+
+
+
+      // Publish the profile photo to NanoStore
+      // const uploadResult = await publishFile({
+      //   config: {
+      //     nanostoreURL: NANOSTORE_URL,
+      //     clientPrivateKey: SERVER_PRIVATE_KEY,
+      //     dojoURL: DOJO_URL
+      //   },
+      //   file: {
+      //     dataAsBuffer: selfiePhotoBuffer,
+      //     size: selfiePhotoBuffer.length,
+      //     type: 'image/jpeg'
+      //   },
+      //   retentionPeriod
+      // })
+
+      // if (!uploadResult) {
+      //   return res.status(400).json({
+      //     status: 'notVerified',
+      //     description: 'Failed to upload profile photo!'
+      //   })
+      // }
+
+      if (!dataResponse || dataResponse.status != 200) {
+        return res.status(400).json({ // 204 might be better
+          status: 'notVerified',
+          description: 'User identity has not been verified!'
         })
+      }
 
-        let access_token = authResponse.data.access_token;
-        const dataResponse = await axios.get(`${DISCORD_API_ENDPOINT}/oauth2/@me`, { headers: {'Authorization': `Bearer ${access_token}`}});
+      const userData = {
+        userName: dataResponse.username,
+        profilePhoto: `https://cdn.discordapp.com/avatars/${dataResponse.id}}/${dataResponse.avatar}.png`
+      }
+
+      return res.status(200).json({
+        status: 'verified',
+        description: 'User identity is verified!',
+        verifiedAttributes: userData
+      })
 
 
-      
 
-          // Publish the profile photo to NanoStore
-          // const uploadResult = await publishFile({
-          //   config: {
-          //     nanostoreURL: NANOSTORE_URL,
-          //     clientPrivateKey: SERVER_PRIVATE_KEY,
-          //     dojoURL: DOJO_URL
-          //   },
-          //   file: {
-          //     dataAsBuffer: selfiePhotoBuffer,
-          //     size: selfiePhotoBuffer.length,
-          //     type: 'image/jpeg'
-          //   },
-          //   retentionPeriod
-          // })
 
-          // if (!uploadResult) {
-          //   return res.status(400).json({
-          //     status: 'notVerified',
-          //     description: 'Failed to upload profile photo!'
-          //   })
-          // }
-
-          if(!dataResponse || dataResponse.status != 200){
-            return res.status(400).json({ // 204 might be better
-              status: 'notVerified',
-              description: 'User identity has not been verified!'
-            })
-          }
-
-          const userData = {
-            userName: dataResponse.username, 
-            profilePhoto: `https://cdn.discordapp.com/avatars/${dataResponse.id}}/${dataResponse.avatar}.png`
-          }
-
-          return res.status(200).json({
-            status: 'verified',
-            description: 'User identity is verified!',
-            verifiedAttributes: userData
-          })
-        
-      
-
-      
     } catch (e) {
       console.error(e)
       res.status(500).json({
